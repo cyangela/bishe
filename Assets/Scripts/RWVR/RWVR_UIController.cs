d38fe39cb1d821da6f4c516bc0e540b8b84c28dc
cyangela <47617679+cyangela@users.noreply.github.com>
2019-05-17 14:41:03 +0800

    5.17.14.40
    提交动物细胞详细，添加设计思路

---
+++ Assets/Scripts/RWVR/RWVR_UIController.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RWVR_UIController : RWVR_InteractionObject
+{
+
+    private void Start()
+    {
+        Debug.Log(GetComponent<Button>().onClick.GetPersistentMethodName(0));
+    }
+    public override void OnTriggerWasPressed(RWVR_InteractionController controller)
+    {
+        GetComponent<Button>().Select();
+    }
+}
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -2,26 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum RotateDirection
+{
+    Forward,
+    Up,
+    Left,
+}
+
 public class Rotate : MonoBehaviour {
+    [Header("自转")]
+    public bool isSelfRotate;
 
-    public Transform tf1;
-    public Transform tf2;
-    float h;
+    [Header("顺时针自转")]
+    public bool isSelfRotatePositive;
+
+    [Header("自转速度")]  
+    [Range(1f, 100f)]
+    public float speed;
+
     float t;
+    [Header("漂浮方向")]
+    public RotateDirection rotateDirection = RotateDirection.Forward;
 
-	void Start ()
+    [Header("漂浮幅度")]
+    [Range(-0.1f, 0.1f)]
+    public float range = 0.1f;
+
+    float hight;
+    void Start ()
     {
-        h = tf1.position.y;
+        if (rotateDirection == RotateDirection.Forward)
+        {
+            hight = transform.localPosition.z;
+        }
+        else if (rotateDirection == RotateDirection.Left)
+        {
+            hight = transform.localPosition.x;
+        }
+        else
+        {
+            hight = transform.localPosition.y;
+        }
     }
 	
 	void Update ()
     {
         t += Time.deltaTime;
-        tf1.transform.RotateAround(transform.position, transform.up, 40 * Time.deltaTime);
-        tf1.transform.position = new Vector3(tf1.transform.position.x, h + 0.15f * Mathf.Sin(t), tf1.transform.position.z);
-        tf1.rotation = Quaternion.Euler(0, 0, 0);
-        tf2.transform.RotateAround(transform.position, transform.up, 40 * Time.deltaTime);
-        tf2.rotation = Quaternion.Euler(0, 0, 0);
-        tf2.transform.position = new Vector3(tf2.transform.position.x, h + 0.15f * -Mathf.Sin(t), tf2.transform.position.z);
+        if (rotateDirection == RotateDirection.Forward)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, hight + range * Mathf.Sin(t));
+        }
+        else if(rotateDirection == RotateDirection.Left)
+        {
+            transform.localPosition = new Vector3(hight + range * Mathf.Sin(t), transform.localPosition.y, transform.localPosition.z);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, hight + range * Mathf.Sin(t), transform.localPosition.z);
+        }
+        if (isSelfRotate)
+        {
+            if (isSelfRotatePositive)
+                transform.rotation = Quaternion.Euler(speed * t, speed * t, speed * t);
+            else
+                transform.rotation = Quaternion.Euler(speed * t, -speed * t, speed * t);           
+        }     
     }
 }
